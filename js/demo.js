3a90107a0f9086cfb014e808d9b487dd9f7f2743
Lech Rozanski <lrozanski@soldevelo.com>
2014-02-11 14:36:25 +0100

    Minor formatting; modified gitignore

--- js/demo.js
+++ js/demo.js
@@ -3,5 +3,15 @@ var ms;
 $(document).ready(function() {
     "use strict";
 
-    ms = $("div#container").multiselect({contactItemDisplayLimit: 4, groupItemDisplayLimit: 1, smartgroupItemDisplayLimit: 1}, {"en_US": {"common.item.limit.label": "Showing %s out of %s matches"}});
+    var options = {
+        contactItemDisplayLimit: 4,
+        groupItemDisplayLimit: 1,
+        smartgroupItemDisplayLimit: 1
+    };
+    var translations = {
+        "en_US": {
+            "common.item.limit.label": "Showing %s out of %s matches"
+        }
+    };
+    ms = $("div#container").multiselect(options, translations);
 });
